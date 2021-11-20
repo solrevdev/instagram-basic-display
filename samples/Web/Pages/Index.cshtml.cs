a195550699db4ee1dadf460f27f3e150734fb260
solrevdev <john@solrevdev.com>
2021-11-20 22:03:20 +0000

    net6.0 upgrade

--- samples/Web/Pages/Index.cshtml.cs
+++ samples/Web/Pages/Index.cshtml.cs
@@ -4,7 +4,9 @@ namespace Web.Pages
 {
     public class IndexModel : PageModel
     {
+#pragma warning disable CA1822 // Mark members as static
         public void OnGet()
+#pragma warning restore CA1822 // Mark members as static
         {
             // nothing to do here
         }
--- samples/Web/Startup.cs
+++ samples/Web/Startup.cs
@@ -38,7 +38,9 @@ namespace Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+#pragma warning disable CA1822 // Mark members as static
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+#pragma warning restore CA1822 // Mark members as static
         {
             if (env.IsDevelopment())
             {
